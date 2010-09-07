768f52c621a82d24d66b8a920bb05d6c1d831303
Ondrej Certik <ondrej@certik.cz>
2010-09-07 16:48:34 -0700

    Use RCP in the example

--- examples/rcp/main.cpp
+++ examples/rcp/main.cpp
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include "Teuchos_RCP.hpp"
+
+using Teuchos::RCP;
+using Teuchos::rcp;
+
+class A {
+public:
+    A() {}
+    ~A() {}
+
+    void test() {
+        printf("A.test()\n");
+    }
+};
+
+RCP<A> create_A() {
+    RCP<A> a;
+    a = rcp(new A());
+    return a;
+}
 
 int main()
 {
-    printf("OK\n");
+    RCP<A> a = create_A();
+    a->test();
     return 0;
 }
