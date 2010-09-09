73c78296689274d8f7d3a3c79f2f460e3cd928c0
Ondrej Certik <ondrej@certik.cz>
2010-09-08 23:39:47 -0700

    args example added

---
+++ examples/args/main.cpp
@@ -0,0 +1,109 @@
+#include "Teuchos_RCP.hpp"
+
+// Inject symbols for RCP so we don’t need Teuchos:: qualification
+using Teuchos::RCP;
+using Teuchos::rcp; // Warning! This can be dangerous and is not to be used in general!
+using Teuchos::Ptr;
+
+// Abstract interfaces
+class UtilityBase {
+public:
+    virtual void f() const = 0;
+};
+
+class UtilityBaseFactory {
+public:
+    virtual RCP<UtilityBase> createUtility() const = 0;
+};
+
+// Concrete implementations
+class UtilityA : public UtilityBase {
+public:
+    void f() const {
+        std::cout << "\nUtilityA::f() called, this=" << this << "\n";
+    }
+};
+
+class UtilityB : public UtilityBase {
+public:
+    void f() const {
+        std::cout << "\nUtilityB::f() called, this=" << this << "\n";
+    }
+};
+
+class UtilityAFactory : public UtilityBaseFactory {
+public:
+    RCP<UtilityBase> createUtility() const {
+        return rcp(new UtilityA());
+    }
+};
+
+class UtilityBFactory : public UtilityBaseFactory {
+public:
+    RCP<UtilityBase> createUtility() const {
+        return rcp(new UtilityB());
+    }
+};
+
+// Client classes
+class ClientA {
+public:
+    void f(const UtilityBase &utility) const {
+        utility.f();
+    }
+};
+
+class ClientB {
+    RCP<UtilityBase> utility_;
+public:
+    void initialize(const RCP<UtilityBase> &utility) {
+        utility_ = utility;
+    }
+    void g(const ClientA &a) {
+        a.f(*utility_);
+    }
+};
+
+class ClientC {
+    RCP<const UtilityBaseFactory> utilityFactory_;
+    RCP<UtilityBase> utility_;
+    bool shareUtility_;
+public:
+    ClientC(const RCP<const UtilityBaseFactory> &utilityFactory,
+            bool shareUtility)
+        :utilityFactory_(utilityFactory),
+            utility_(utilityFactory->createUtility()),
+            shareUtility_(shareUtility) {}
+
+    void h(const Ptr<ClientB> &b) {
+        if(shareUtility_)
+            b->initialize(utility_);
+        else
+            b->initialize(utilityFactory_->createUtility());
+    }
+};
+
+// Main program
+int main(int argc, char* argv[])
+{
+    // Read options from the commandline
+    bool useA, shareUtility;
+    //example_get_args(argc,argv,&useA,&shareUtility);
+    // Create factory
+    RCP<UtilityBaseFactory> utilityFactory;
+    useA = true;
+    shareUtility = true;
+    if (useA)
+        utilityFactory = rcp(new UtilityAFactory());
+    else
+        utilityFactory = rcp(new UtilityBFactory());
+    // Create clients
+    ClientA a;
+    ClientB b1, b2;
+    ClientC c(utilityFactory, shareUtility);
+    // Do some stuff
+    c.h(&b1);
+    c.h(&b2);
+    b1.g(a);
+    b2.g(a);
+}
