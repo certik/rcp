226e7cac4c002940915180b5e026e1d273119232
Ondrej Certik <ondrej@certik.cz>
2010-09-24 12:48:40 -0700

    Fix a memory leak

--- src/Teuchos_stacktrace.cpp
+++ src/Teuchos_stacktrace.cpp
@@ -249,6 +249,10 @@ int load_symbol_table(bfd *abfd, line_data *data)
     unsigned int symbol_size;
     n_symbols = bfd_read_minisymbols(abfd, false, tmp, &symbol_size);
     if (n_symbols == 0) {
+        // If the bfd_read_minisymbols() already allocated the table, we need
+        // to free it first:
+        if (data->symbol_table != NULL)
+            free(data->symbol_table);
         // dynamic
         n_symbols = bfd_read_minisymbols(abfd, true, tmp, &symbol_size);
     }
